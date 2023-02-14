247190795565bf26ee6ef5b49009d2bf95d770d3
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-14 20:27:45 +0200

    build: add examples

---
+++ examples/about_author/author_name.js
@@ -0,0 +1,4 @@
+import fname from './author_first_name.js'
+import lname from './author_last_name.js'
+
+export default fname + ' ' + lname;
---
+++ examples/about_author/entry.js
@@ -0,0 +1,7 @@
+import name from './author_name.js';
+import country from './author_country.js'
+
+console.log({
+  name: name,
+  country: country
+})
